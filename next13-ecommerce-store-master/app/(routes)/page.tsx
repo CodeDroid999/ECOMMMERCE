9d710a35d8f804e0a3d9a9e74cd503d4718c1103
CodeDroid99 <johnmwega2002@gmail.com>
2024-03-18 15:55:57 +0300

    update pages/index.tsx

--- next13-ecommerce-store-master/app/(routes)/page.tsx
+++ next13-ecommerce-store-master/app/(routes)/page.tsx
@@ -8,19 +8,18 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = "https://i.postimg.cc/zvGPPq4M/pexels-soulful-pizza-3780680-1.jpg"
+  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard
+        <Billboard 
           data={billboard}
         />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
         </div>
       </div>
-      <div className="text-black">Hello</div>
     </Container>
   )
 };
